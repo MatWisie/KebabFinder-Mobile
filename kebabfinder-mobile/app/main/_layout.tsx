474d68e0d06b219a7316e3b8d5de36e5d07f7495
MatWisie <Mateuszwisiecki@gmail.com>
2024-11-20 22:59:56 +0100

    In more view show header

--- kebabfinder-mobile/app/main/_layout.tsx
+++ kebabfinder-mobile/app/main/_layout.tsx
@@ -4,6 +4,7 @@ export default function MainLayout() {
     return (
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="more" options={{ headerShown: true }} />
       </Stack>
     );
   }
