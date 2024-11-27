39e881cd778538b72b7441ee683e4c59c98f668e
MatWisie <Mateuszwisiecki@gmail.com>
2024-11-27 13:51:19 +0100

    Rearranged _layout in main

--- kebabfinder-mobile/app/main/_layout.tsx
+++ kebabfinder-mobile/app/main/_layout.tsx
@@ -1,10 +1,17 @@
-import { Stack, Slot } from "expo-router";
+import { Stack, Slot, Tabs } from "expo-router";
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 
 export default function MainLayout() {
     return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="more" options={{ headerShown: true }} />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="more"
+        options={{
+          headerShown: true, 
+          title: "More Details",
+        }}
+      />
+    </Stack>
     );
   }
